4e07bb282436d2a1fe6e95faed83f13514fdf7b7
RadMom <RadMom1111@gmail.com>
2023-01-23 16:07:11 +0200

    MongoDB added ....REST & CRUD operations

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
-console.log("Hello server!");
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+const phonebookRoutes = require("./routes/contactsRoutes");
+
+const port = 3000;
+const URL = process.env.MONGO_URL;
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) =>
+    app.listen(3000, () => {
+      console.log(`server is live on port: ${port}...`);
+    })
+  )
+  .catch((err) => console.log(err));
+
+const app = express();
+app.use(express.json()); //In old version is body-parser.Server can accept json in the body of the req.
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/contacts", phonebookRoutes);
+
+app.use(errorHandler);
